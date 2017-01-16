f69506af099bc2cdb9e48d038b0b8daf61a00854
tankian tankian <tankian tankian>
2017-01-16 14:57:47 +0200

    switches

---
+++ switch_app.js
@@ -0,0 +1,14 @@
+var my_name = "lonzo"
+
+switch(my_name){
+    case "moshe":console.log(my_name+" is not my name!");
+    break;
+
+    case "jack":console.log(my_name+" is not my name!");
+        break;
+
+    case "asaf":console.log("thats me!");
+        break;
+
+    default: console.log("hey! you dont know my name!")
+}
